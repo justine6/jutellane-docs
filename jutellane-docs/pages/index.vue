b1263f2f024fc1c6086e966a2462bcdb2dd7e90b
JustineLongla <107580556+JustineLongla@users.noreply.github.com>
2025-11-03 19:31:43 -0600

    docs: restore pure-static HTML + CSS; remove frameworks

--- jutellane-docs/pages/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="prose dark:prose-invert max-w-none">
-    <h1>Welcome to Jutellane Docs</h1>
-    <p>This site documents our tooling, pipelines, and practices.</p>
-    <p>Jump to the <NuxtLink to="/guide/getting-started">Getting Started</NuxtLink> guide.</p>
-  </div>
-</template>
